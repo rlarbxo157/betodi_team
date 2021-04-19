01a7828ae85ac9b4628be1e80394300a51889c12
20140108 <rlarbxo157@naver.com>
2021-04-19 23:26:03 +0900

    cookie 인증, cors 해결

--- server/index.js
+++ server/index.js
@@ -1,10 +1,25 @@
 const express = require('express');
+const {User} = require("./Model/User");
+
+
 
 const app = express();
 const PORT =process.env.PORT || 8000;
+const mongoose = require('mongoose');
+const cookieParser =require('cookie-parser');
+const {auth}  =require('./middleware/auth');
+const cors = require('cors')  //cors 에러 해결하기위해
+
+mongoose.connect('mongodb+srv://rlarbxo157:""@cluster0.7xilp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
+    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+}).then(()=>console.log('몽고디비 연결')).catch((err)=>console.log(err));
+
+
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());  //
+app.use(cookieParser());
+app.use(cors())
 
 
 app.use('/api/users',require('./routes/users'));
