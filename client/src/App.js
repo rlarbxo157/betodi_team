f4f9d5a180f41705ab4816b1163db5669a556626
20140108 <rlarbxo157@naver.com>
2021-04-26 23:32:52 +0900

    no message

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,34 @@
+import React,{ Suspense } from 'react';
 import './App.css';
+import {
+  Switch,
+  Route,
+} from "react-router-dom";
+import LandingPage from './components/views/LandingPage/LandingPage';
+import LoginPage from './components/views/LoginPage/LoginPage';
+import RegisterPage from './components/views/RegisterPage/RegisterPage';
+import Auth from './hoc/auth';
+import Navbar from './components/views/Navbar/Navbar';
+import Footer from './components/views/Footer/Footer';
+import BookUploadPage from './components/views/BookUploadPage/BookUploadPage';
+import DetailProductPage from './components/views/DetailProductPage/DetailProductPage'
 
 function App() {
   return (
-    <div className="App">
+    <Suspense fallback={(<div>Loading...</div>)}>
+      <Navbar />
+      <div style={{ paddingTop: '69px', minHeight: 'calc(100vh - 80px)' }}>
+        <Switch>
+          <Route exact path="/" component={Auth(LandingPage, null)} />
+          <Route exact path="/login" component={Auth(LoginPage, false)} />
+          <Route exact path="/register" component={Auth(RegisterPage, false)} />
+          <Route exact path="/product/upload" component={Auth(BookUploadPage, true)} />
+          <Route exact path="/product/:productId" component={Auth(DetailProductPage, null)} />
 
-    </div>
+        </Switch>
+      </div>
+      <Footer />
+    </Suspense>
   );
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -4,9 +4,29 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+// import * as serviceWorker from './serviceWorker';
+import { BrowserRouter } from "react-router-dom";
+
+import {Provider} from 'react-redux';
+import { applyMiddleware,createStore } from 'redux';
+import promiseMiddleware from 'redux-promise';
+import ReduxThunk from 'redux-thunk';
+import Reducer from './_reducers'
+
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware,ReduxThunk)(createStore)
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={createStoreWithMiddleware(Reducer,
+        window.__REDUX_DEVTOOLS_EXTENSION__&&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+      )}
+    >
+      <BrowserRouter>
+          <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
