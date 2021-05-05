69053a1e2a0430dfe89322e881eeb7c78f03af24
20150223 <20150223@kumoh.ac.kr>
2021-05-06 08:28:57 +0900

    몽고디비 로그인

--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const {User} = require("./Model/User");
+const { User } = require("./Model/User");
 
 const app = express();
-const PORT =process.env.PORT || 8000;
+const PORT = process.env.PORT || 8000;
 const path = require("path");
 const mongoose = require('mongoose');
-const cookieParser =require('cookie-parser');
-const {auth}  =require('./middleware/auth');
+const cookieParser = require('cookie-parser');
+const { auth } = require('./middleware/auth');
 const cors = require('cors')
 app.use('/uploads', express.static('uploads'));
 const axios = require('axios');
@@ -14,9 +14,11 @@ const cheerio = require('cheerio');
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0
 
 
-mongoose.connect('mongodb+srv://rlarbxo157:sjaks7788@cluster0.7xilp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
-    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
-}).then(()=>console.log('몽고디비 연결')).catch((err)=>console.log(err));
+mongoose.connect('mongodb+srv://ksh5681:ksh5681@cluster0.a8wub.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+}).then(() => console.log('몽고디비 연결')).catch((err) => console.log(err));
+//mongodb+srv://rlarbxo157:sjaks7788@cluster0.7xilp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -27,7 +29,7 @@ app.use(cors())
 // app.get('/',(req,res)=> res.send('접속됨'))
 
 app.use('/api/users', require('./routes/users'));
-app.use('/api/product',require('./routes/product'));
+app.use('/api/product', require('./routes/product'));
 
 app.use('/uploads', express.static('uploads'));
 
@@ -45,21 +47,21 @@ if (process.env.NODE_ENV === "production") {
 }
 
 app.listen(PORT, () => {
-    console.log(`example app ${PORT} `);
+  console.log(`example app ${PORT} `);
 })
 
-app.get('/api/hello',(req,res)=> {
+app.get('/api/hello', (req, res) => {
   res.send('안녕하세요')
 })
 
-const getHtml = async (word) =>{
+const getHtml = async (word) => {
 
   let wordUrl = word;
-  try{
-      // return await axios.get('https://www.aladin.co.kr/search/wsearchresult.aspx?SearchTarget=All&SearchWord=%EC%9A%B4%EC%98%81%EC%B2%B4%EC%A0%9C&x=0&y=0')
-      return await axios.get(`https://www.aladin.co.kr/search/wsearchresult.aspx?SearchTarget=All&SearchWord=${wordUrl}&x=0&y=0`);
-  } catch(error){
-      console.log(error);
+  try {
+    // return await axios.get('https://www.aladin.co.kr/search/wsearchresult.aspx?SearchTarget=All&SearchWord=%EC%9A%B4%EC%98%81%EC%B2%B4%EC%A0%9C&x=0&y=0')
+    return await axios.get(`https://www.aladin.co.kr/search/wsearchresult.aspx?SearchTarget=All&SearchWord=${wordUrl}&x=0&y=0`);
+  } catch (error) {
+    console.log(error);
   }
 }
 
@@ -77,11 +79,11 @@ app.post("/craw", (req, res) => {
       let resultArr = [];
       parentTag.each(function (i, elem) {
         let itemObj = {
-          platform:'알라딘',
-          title:$(this).find('a.bo3 b').text(),
+          platform: '알라딘',
+          title: $(this).find('a.bo3 b').text(),
           price: $(this).find("b span").text(),
-          imgEl : $(this).find("img").attr('src'),
-          link : $(this).find("div.ss_book_list a").attr('href')
+          imgEl: $(this).find("img").attr('src'),
+          link: $(this).find("div.ss_book_list a").attr('href')
           // img: $(this).find("img")
         };
         resultArr.push(itemObj);
@@ -100,7 +102,7 @@ app.post("/craw", (req, res) => {
 //   let wordUrl = word;
 //   try{
 //     return await axios.post(`https://www.littlemom.co.kr:8443/sub/01/srclist_book.html?keyfield=item_name&key=${wordUrl}`);
-    
+
 
 //   } catch(error){
 //       console.log(error);
@@ -108,12 +110,12 @@ app.post("/craw", (req, res) => {
 // }
 
 // app.post("/yes", (req, res) => {
-  
+
 //   let newWord = req.body.searchData;
 //   let a = iconv.encode(newWord,'euc-kr');
 //   let b= a.toString('hex');
 //   let aa = b.toUpperCase();
-  
+
 //   let bb ="%";
 //   let str = aa.replace(/(.{2})/g,"$1%");
 //   let newStr = bb.concat(str);
@@ -136,7 +138,7 @@ app.post("/craw", (req, res) => {
 //         };
 //         console.log(itemObj);
 //         resultArr.push(itemObj);
-        
+
 //       });
 //       return resultArr;
 //     })
@@ -148,6 +150,6 @@ app.post("/craw", (req, res) => {
 // res.send(data)
 
 
-app.get('/',(req,res)=> {
+app.get('/', (req, res) => {
   res.send('hihi')
 })
