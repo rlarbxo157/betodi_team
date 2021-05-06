cc6862d0287d8d241b8c9a0e7bca387123c3912c
20150223 <20150223@kumoh.ac.kr>
2021-05-06 09:12:45 +0900

    상품 상세정보에서 북카드 추가

--- client/src/components/views/DetailProductPage/Sections/ProductInfo.js
+++ client/src/components/views/DetailProductPage/Sections/ProductInfo.js
@@ -1,9 +1,16 @@
-import { Descriptions,Button } from 'antd'
-import React from 'react'
-
+import { Descriptions, Button } from 'antd'
+import React, { useState, useEffect } from 'react'
+import Item from '../../BookUploadPage/Item';
 
 function ProductInfo(props) {
-    return(
+
+    const [items, setItems] = useState([]);
+
+    useEffect(() => {
+        setItems(props.detail.selectedbooks);
+    }, [props.detail]);
+
+    return (
         <div>
             <Descriptions title="도서 정보" bordered>
                 <Descriptions.Item label="Price">{props.detail.price}</Descriptions.Item>
@@ -17,13 +24,28 @@ function ProductInfo(props) {
             <br />
             <br />
 
-            
-            <div style={{display:'flex', justifyContent:'center'}}>
+
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
                 <Button size="large" shape="round" type="danger" onClick>
                     구매 문의하기
                 </Button>
             </div>
-            
+
+
+            {items && items.length > 0 ? items.map((selectedBook, index) => (
+                <Item
+                    key={index}
+                    thumbnail={selectedBook.thumbnail}
+                    title={selectedBook.title}
+                    price={selectedBook.price}
+                    isbn={selectedBook.isbn}
+                    publisher={selectedBook.publisher}
+                    price={selectedBook.price}
+                    authors={selectedBook.authors}
+                    //contents={book.contents}
+                    url={selectedBook.url}
+                />
+            )) : "Loading..."}
         </div>
     )
 }
