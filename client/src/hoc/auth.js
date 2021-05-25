5836b2ca35fbfbb61427cdf5ca53032039f44a02
20150223 <20150223@kumoh.ac.kr>
2021-05-26 01:25:55 +0900

    이미지 , 환경변수, 패키지 파일

--- client/src/hoc/auth.js
+++ client/src/hoc/auth.js
@@ -1,34 +1,34 @@
-import React,{useEffect} from 'react';
-import {useDispatch,useSelector} from 'react-redux';
-import {auth} from '../_actions/user_action';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { auth } from '../_actions/user_action';
 
 
-export default function (SpecificComponent,option,adminRoute=null){
+export default function (SpecificComponent, option, adminRoute = null) {
 
 
-    function AuthenticationCheck(props){
+    function AuthenticationCheck(props) {
         let user = useSelector(state => state.user);
         const dispatch = useDispatch();
 
         useEffect(() => {
-           dispatch(auth()).then(response=> {
+            dispatch(auth()).then(response => {
                 console.log(response);
 
-                if(!response.payload.isAuth){  //로그인하지않은상태
-                    if(option){
+                if (!response.payload.isAuth) {  //로그인하지않은상태
+                    if (option) {
                         props.history.push('/login')
-                    }    
-                }else{
-                            //로그인한상태
-                    if(adminRoute&& !response.payload.isAdmin ){
+                    }
+                } else {
+                    //로그인한상태
+                    if (adminRoute && !response.payload.isAdmin) {
                         props.history.push('/');
-                    }else{
-                        if(option===false){
+                    } else {
+                        if (option === false) {
                             props.history.push('/');
                         }
                     }
                 }
-           })
+            })
         }, [])
         return (
             <SpecificComponent {...props} user={user} />
