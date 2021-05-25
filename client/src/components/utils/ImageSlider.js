8cf2a6bc7c223b6ab3d12e4355f4339609e6d570
20150223 <20150223@kumoh.ac.kr>
2021-05-26 00:58:10 +0900

    이미지 슬라이더 css 수정

--- client/src/components/utils/ImageSlider.js
+++ client/src/components/utils/ImageSlider.js
@@ -1,17 +1,17 @@
 import React from 'react'
-import { Col,Card,Row,Carousel} from 'antd';
+import { Col, Card, Row, Carousel } from 'antd';
 import Icon from '@ant-design/icons';
 import Meta from 'antd/lib/card/Meta';
+import "./imageslider.css"
 
 function ImageSlider(props) {
     console.log(props);
-    return(
+    return (
         <div>
             <Carousel autoplay>
-                {props.images.map((data,index)=> (
+                {props.images.map((data, index) => (
                     <div key={index}>
-                        <img style={{width:'100%',height:'200px', maxHeight:'250px'}}
-                         src={`http://localhost:8000/${data}`} />
+                        <img className="thumbnail" src={`http://localhost:8000/${data}`} />
                     </div>
                 ))}
             </Carousel>
