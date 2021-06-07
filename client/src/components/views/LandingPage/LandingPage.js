90d2713b033ee446a37723ee4e48e688c250f9e6
20150223 <20150223@kumoh.ac.kr>
2021-06-07 12:18:07 +0900

    백탑 추가

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -10,6 +10,18 @@ import SearchFeature from './Sections/SearchFeature';
 import CheckBox from './Sections/CheckBox'
 import { major, price } from './Sections/Datas';
 import RadioBox from './Sections/RadioBox';
+import { BackTop } from 'antd';
+
+const style = {
+    height: 60,
+    width: 60,
+    lineHeight: '60px',
+    borderRadius: 50,
+    backgroundColor: '#1088e9',
+    color: '#fff',
+    textAlign: 'center',
+    fontSize: 20,
+};
 
 function LandingPage(props) {
     // useEffect(()=> {
@@ -174,6 +186,9 @@ function LandingPage(props) {
                 {/* <h1>외부 사이트 중고서적</h1> */}
                 <BookCrawling />
             </div>
+            <BackTop>
+                <div style={style}>UP</div>
+            </BackTop>
         </>
     )
 }
